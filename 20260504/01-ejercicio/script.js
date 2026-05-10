2a56453d64dd74fe1bdf4725148a0838a09922b3
isabel <tecwebisa7@gmail.com>
2026-05-10 23:27:24 +0200

    actualizacion

--- 20260504/01-ejercicio/script.js
+++ 20260504/01-ejercicio/script.js
@@ -1,20 +1,19 @@
-console.log("¡El cerebro de la web está funcionando!");
-// Función saludar
+console.log("! El cerebro de la web esta funcionando !");
+//Funcion saludar
 function saludar(){
-    let nombre = prompt("¿Cómo te llamas?");
-    alert("¡Hola " + nombre + " ! Ya eres programador/a.");
+    let nombre = prompt("¿Como te llamas?");
+    alert("!Hola " + nombre +"! Ya eres programador/a.");
 }
-
-// Funcion Condicional
+// Funcion verificar acceso
 function verificarAcceso() {
     let edad = prompt("Introduce tu edad :");
 
     if (edad >=18) {
-        alert("¡Acceso permitido!");
+        alert("!Acceso permitido");
         console.log("Estado mayor de edad");
-    } else {
-        alert("¡Acceso denegado! Eres menor.");
-        console.log("Estado: Menor de edad detectado.");
     }
-
-}
+        else{
+            alert("!Acceso denegado! Eres menor.");
+            console.log("Estado: Menor de edad detectado");
+        }
+    }
