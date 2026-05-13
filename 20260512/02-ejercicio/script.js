a75293e5a68c4b237ae7a3f1e247201360133202
isabeltecweb <tecwebisa7@gmail.com>
2026-05-13 19:47:43 +0200

    update 20260512

---
+++ 20260512/02-ejercicio/script.js
@@ -0,0 +1,7 @@
+//1. NUESTRO SÚPER ARRAY (Array lleno de objetos)
+const carrito = [
+    {nombre: "🍞 Pan de molde", precio:1.20 },
+    {nombre: "🥛 Leche entera", precio: 0.90},
+    {nombre: "🥚 Huevos Camperos", precio: 2.50},
+    {nombre: "🥑 Aguacate", precio:1.00}
+];
