fb5dfd8ae3dd58190fb26e8b03fa2f87b5daa232
isabel <tecwebisa7@gmail.com>
2026-05-15 23:29:41 +0200

    update 20260512

--- 20260512/02-ejercicio/script.js
+++ 20260512/02-ejercicio/script.js
@@ -5,3 +5,27 @@ const carrito = [
     {nombre: "🥚 Huevos Camperos", precio: 2.50},
     {nombre: "🥑 Aguacate", precio:1.00}
 ];
+//CÓDIGO DE APOYO VISUAL
+let listaHTML = document.getElementById('lista-producto');
+for(let i = 0; i < carrito.length;i++){
+    // Usamos carrito[i].nombre para sacar el dato en cada vuelta
+    listaHTML.innerHTML += `
+    <li><span>${carrito[i].nombre}</span>
+    <span>${carrito[i].precio.toFixed(2)}€</span>
+    `  
+}
+// LA FUNCION COBRAR
+function cobrar() {
+//1. Creamos una variables = acumulador
+let sumaTotal = 0;
+//2. Creamos un bucle for para recorrer el array
+for (let i = 0; i< carrito.length; i++){
+    //En cada vuelta le sumamos al "sumaTotal" el precio
+    sumaTotal = sumaTotal + carrito[i].precio;
+}
+
+//3. Mostramos el resultado final en HTML
+document.getElementById('resultado-total').textContent =
+"Total: " + sumaTotal.toFixed(2) + " €";
+
+}
