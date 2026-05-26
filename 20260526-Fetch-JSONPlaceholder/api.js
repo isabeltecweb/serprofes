0296c2df48293b08b12129575eb7c6ee84842f4b
isabeltecweb <tecwebisa7@gmail.com>
2026-05-26 21:24:16 +0200

    20260526

---
+++ 20260526-Fetch-JSONPlaceholder/api.js
@@ -0,0 +1,7 @@
+import { baseURL } from "./config";
+
+/*callAPI: Nuestro cartero virtual (Cliente Generico)
+* @param {String} ruta - La ruta final(ej. "/post/1")
+* @param {object} opciones - Configuracion extra (POST, DELETE...)
+*/
+
